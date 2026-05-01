82f12ddd2edf54bd404d3ce94c94aca817f28d2d
userName <userEmail>
2026-05-01 11:46:19 +0800

    first commit

--- TEST.sh
+++
@@ -1,3 +0,0 @@
-python3 scripts/batch.py --vehN-list 50,60,70,80 --pstep 0.1 --seeds 1 --outcsv out/TEST.csv
-python3 DataVisualization.py --csv out/TEST.csv
-echo "[OK] 检验产物：./out/TEST.csv、./graph/*"
